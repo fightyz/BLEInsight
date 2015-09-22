1a4b65ca1859bb4560c8651889a8269b7d1c1f98
Yang Zhou <yangzhou@std.uestc.edu.cn>
2015-09-22 20:55:01 +0800

    修改了一些布局

--- app/src/main/java/org/esec/mcg/bleinsight/PeripheralActivity.java
+++ app/src/main/java/org/esec/mcg/bleinsight/PeripheralActivity.java
@@ -1,11 +1,17 @@
 package org.esec.mcg.bleinsight;
 
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.esec.mcg.bleinsight.wrapper.BLEWrapper;
+import org.esec.mcg.utils.logger.LogUtils;
+import org.w3c.dom.Text;
 
 public class PeripheralActivity extends AppCompatActivity {
 
@@ -14,11 +20,36 @@ public class PeripheralActivity extends AppCompatActivity {
     public static final String EXTRAS_DEVICE_RSSI       = "BLE_DEVICE_RSSI";
     public static final String EXTRAS_DEVICE_BOND       = "BLE_DEVICE_BOND";
 
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private String mDeviceRSSI;
+
+    private BLEWrapper mBleWrapper;
+
+    private TextView mDeviceNameView;
+    private TextView mDeviceAddressView;
+    private TextView mDeviceRssiView;
+    private TextView mDeviceStatusView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peripheral);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        connectViewsVariables();
+
+        final Intent intent = getIntent();
+        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
+        LogUtils.d(mDeviceName);
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+        mDeviceRSSI = intent.getStringExtra(EXTRAS_DEVICE_RSSI);
+        mDeviceNameView.setText(mDeviceName);
+        mDeviceAddressView.setText(mDeviceAddress);
+        mDeviceRssiView.setText(mDeviceRSSI);
+        // TODO
+        // 这里actionBar有问题？
+//        getActionBar().setTitle(mDeviceName);
     }
 
     @Override
@@ -42,5 +73,13 @@ public class PeripheralActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void connectViewsVariables() {
+        mDeviceNameView = (TextView) findViewById(R.id.peripheral_name);
+        mDeviceAddressView = (TextView) findViewById(R.id.peripheral_address);
+        mDeviceRssiView = (TextView) findViewById(R.id.peripheral_rssi);
+        mDeviceStatusView = (TextView) findViewById(R.id.peripheral_status);
+
     }
 }
