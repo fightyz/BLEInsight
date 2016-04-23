ad9f19b4659b1b1503c7e13e95d4ad1aa765f334
Yang Zhou <yangzhou@std.uestc.edu.cn>
2016-04-23 16:43:01 +0800

    Add Android 6.0 bluetooth permission support

--- app/src/main/java/org/esec/mcg/bleinsight/ScanDeviceActivity.java
+++ app/src/main/java/org/esec/mcg/bleinsight/ScanDeviceActivity.java
@@ -1,10 +1,15 @@
 package org.esec.mcg.bleinsight;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.ScanResult;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -15,8 +20,11 @@ import android.widget.Toolbar;
 import org.esec.mcg.bleinsight.adapter.ScanDeviceAdapter;
 import org.esec.mcg.bleinsight.wrapper.BLEWrapper;
 import org.esec.mcg.bleinsight.wrapper.ScanDeviceUiCallbacks;
+import org.esec.mcg.utils.logger.LogUtils;
 
 public class ScanDeviceActivity extends Activity implements ScanDeviceUiCallbacks {
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
     private static final long SCANNING_TIMEOUT = 20 * 1000;  /* 5 seconds */
     private static final int ENABLE_BT_REQUEST_ID = 1;
 
@@ -60,6 +68,23 @@ public class ScanDeviceActivity extends Activity implements ScanDeviceUiCallback
 
         if (mBLEWrapper.checkBleHardwareAvailable() == false) {
             BLEMissing();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
         }
     }
 
@@ -140,5 +165,28 @@ public class ScanDeviceActivity extends Activity implements ScanDeviceUiCallback
     public void uiStopScanning() {
         stopScanningInit();
         invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LogUtils.d("coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+
+                        }
+                    });
+                    builder.show();
+                }
+                break;
+        }
     }
 }
